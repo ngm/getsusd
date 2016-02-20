b8fb08645a4e3e9c53c5f607c5cc48e5e4f07fb6
Neil Mather <ngmather@gmail.com>
2016-02-20 15:53:04 +0000

    Add basic product list to web front end

---
+++ sdg12/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using NHibernate;
+using sdg12.Service.Handlers;
+using sdg12.Service.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sdg12.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ISession nhSession;
+
+        public ProductController(ISession nhSession)
+        {
+            this.nhSession = nhSession;
+        }
+
+        public ActionResult List()
+        {
+            var userId = 1;
+
+            var request = new GetProductsQuery()
+            {
+                UserId = userId
+            };
+
+            var handler = new GetProductsQueryHandler(nhSession);
+            var result = handler.Handle(request);
+
+            return View(result);
+        }
+    }
+}
---
+++ sdg12/Infrastructure/NhHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using FluentNHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using FluentNHibernate.Mapping.Providers;
+using FluentNHibernate.Mapping;
+using sdg12.Data;
+
+namespace sdg12.Infrastructure
+{
+    public static class NhHelper
+    {
+        // http://stackoverflow.com/q/6204511/206297
+        public static FluentMappingsContainer AddFromAssemblyOf<T>(this FluentMappingsContainer mappings, Predicate<Type> where)
+        {
+            if (where == null)
+                return mappings.AddFromAssemblyOf<T>();
+
+            var mappingClasses = typeof(T).Assembly.GetExportedTypes()
+                .Where(x => IsMappingOf<IMappingProvider>(x) ||
+                            IsMappingOf<IIndeterminateSubclassMappingProvider>(x) ||
+                            IsMappingOf<IExternalComponentMappingProvider>(x) ||
+                            IsMappingOf<IFilterDefinition>(x))
+                .Where(x => where(x));
+
+            foreach (var type in mappingClasses)
+            {
+                mappings.Add(type);
+            }
+
+            return mappings;
+        }
+
+        private static bool IsMappingOf<T>(Type type)
+        {
+            return !type.IsGenericType && typeof(T).IsAssignableFrom(type);
+        }
+
+
+        public static ISessionFactory MsSqlSessionFactory
+        {
+            get
+            {
+                return Fluently.Configure()
+                    .Database(MsSqlConfiguration.MsSql2008.ConnectionString(c => c.FromConnectionStringWithKey("TestData")))
+                    .Mappings(cfg => cfg.FluentMappings.AddFromAssemblyOf<UserMap>())
+                    .BuildSessionFactory();
+            }
+        }
+    }
+}
